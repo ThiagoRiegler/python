c8a4211bd17a1abb858a46e4f537f8411b2a24eb
ThiagoRiegler <thiagoriegler123@gmail.com>
2022-08-09 10:27:07 -0300

    a

--- condicional/condicional.py
+++
@@ -1,3 +0,0 @@
-x = int(input("Ingrese un numero: "))
-if x < 100:
-    print("Numero pequeño")
--- condicional/condicional2.py
+++
@@ -1,7 +0,0 @@
-x = int(input("Ingrese el primer numero: "))
-y = int(input("Ingrese el segundo numero: "))
-
-if x < y:
-    print("El menor es:", x)
-else:
-    print("El menor es:", y)
